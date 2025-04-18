480588e47cb960b64a27be052a0e4b9d8e90c713
RayenMbarki <mohamedrayen.mbarki@insat.ucar.tn>
2025-04-18 20:08:06 +0100

    innit commit ray ex1 cv , skill , user

--- ex1/src/app.module.ts
+++ ex1/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { CvModule } from './cv/cv.module';
+import { SkillModule } from './skill/skill.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'rayen266',
+      database: 'nest_tp1',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    CvModule,
+    SkillModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [],
 })
 export class AppModule {}
